0bae24d2ab3a07fccc80bcc2e3514e290a197f50
eumel0815 <danystraub@outlook.de>
2022-07-02 23:13:29 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+  import Tree from './treeComponent/Tree.svelte';
+
+  let root = [
+		{
+			title: 'Important work stuff',
+			disabled: true,
+			children: [
+				{ title: 'quarterly-results.xlsx' }
+			]
+		},
+		{
+			title: 'Animal GIFs',
+			children: [
+				{
+					title: 'Dogs',
+					expanded: true,
+					children: [
+						{ title: 'treadmill.gif' },
+						{ title: 'rope-jumping.gif' }
+					]
+				},
+				{
+					title: 'Goats',
+					children: [
+						{ title: 'parkour.gif' , visible:false},
+						{ title: 'rampage.gif' }
+					]
+				},
+				{ title: 'cat-roomba.gif' },
+				{ title: 'duck-shuffle.gif' },
+				{ title: 'monkey-on-a-pig.gif' }
+			]
+		},
+		{ title: 'TODO.md' }
+	];
+
+let config = {
+		icons:{
+			expanded: 'bi bi-folder2-open',
+			folder: 'bi bi-folder2',
+			document: 'bi bi-file-earmark',
+			disabled: 'bi bi-folder-x'
+		}
+	};
+
+let dblClick = function(event){
+	alert('Clicked' + event.detail.title);
+}
+
+</script>
+
+<svelte:head>
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
+</svelte:head>
+
+<main>
+  <h1>Hello world!</h1>
+
+  <Tree title="Home" treeData={root} expanded visible={true} treeConfig={config} on:treeNodeDoubleClick={dblClick}/>
+ 
+</main>
+
+<style>
+ 
+</style>
---
+++ src/treeComponent/TreeLeaf.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	export let title;
+	export let visible = true;
+	export let iconClass = 'bi bi-file-earmark';
+	
+	//Functions for interacting with the nodes
+	const dispatch = createEventDispatcher();
+	export let treeLeafDoubleClick = function() {
+		dispatch('treeLeafDoubleClick');
+	}
+
+</script>
+
+{#if visible}
+	<span on:dblclick={treeLeafDoubleClick}>
+		<i class={iconClass}></i>
+		<span class="p-2">{title}</span>
+	</span>
+{/if}
+
+<style>
+
+</style>
